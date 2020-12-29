dd41a3d77d26ca20fce6e8b76fd059767ff21694
Timothy Merritt <timmybytes@gmail.com>
2020-12-28 20:29:51 -0800

    (feat): Add info message and show/hide button

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './rational-design-logo-card.svg';
 import githubLogo from './github-logo-icon.svg';
 
-const Header = () => (
-  <header className='header'>
-    <img src={logo} alt='logo' />
-    <a className='header__link' href='https://github.com/timmybytes/rational-design'>
-      <span>Fork Me</span>
-      <img src={githubLogo} alt='Github logo' />
-    </a>
-  </header>
-);
+const Header = () => {
+  const [showInfo, setShowInfo] = useState('none');
+
+  const handleClick = () => {
+    showInfo === 'none' ? setShowInfo('block') : setShowInfo('none');
+  };
+  return (
+    <>
+      <header className='header'>
+        <img className='header__logo' src={logo} alt='logo' />
+        <nav className='header__links'>
+          <a className='header__links-link' href='#' onClick={handleClick}>
+            <span className='header__links-link--span'>Info</span>
+          </a>
+          <a className='header__links-link' href='https://github.com/timmybytes/rational-design'>
+            <span className='header__links-link--span'>Fork Me</span>
+            <img className='header__links-link--icon' src={githubLogo} alt='Github logo' />
+          </a>
+        </nav>
+      </header>
+      <article className='info' style={{ display: showInfo }}>
+        <p>
+          Choose a <strong>ratio</strong>, <strong>base font-size</strong> (pixels), and{' '}
+          <strong>scale</strong> to create a ratio-based type scale! Use the increments of the scale
+          to create projects with pleasing relational sizes.
+        </p>
+        <button onClick={handleClick}>Close</button>
+      </article>
+    </>
+  );
+};
 
 export default Header;
