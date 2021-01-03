104850ac44757bbb67185c7703dcb66306a45d2e
Timothy Merritt <timmybytes@gmail.com>
2021-01-02 21:08:38 -0800

    (refactor): Add descriptions to ratios.js values

--- src/App.js
+++ src/App.js
@@ -54,26 +54,26 @@ function App() {
               className='settings__ratio-select'
               id='ratios-select'
               onChange={handleRatioChange}>
-              <option value={ratios[1]}>Quadrant - 1:{ratios[1]}</option>
-              <option value={ratios[2]}>Hemidiagon - 1:{ratios[2]}</option>
-              <option value={ratios[3]}>Trion - 1:{ratios[3]}</option>
-              <option value={ratios[4]}>Quadriagon - 1:{ratios[4]}</option>
-              <option value={ratios[5]}>Biauron - 1:{ratios[5]}</option>
-              <option value={ratios[6]}>Penton - 1:{ratios[6]}</option>
-              <option value={ratios[7]}>Diagon - 1:{ratios[7]}</option>
-              <option value={ratios[8]}>Bipenton - 1:{ratios[8]}</option>
-              <option value={ratios[9]}>Hemiolion - 1:{ratios[9]}</option>
-              <option value={ratios[10]}>Auron (Golden Ratio) - 1:{ratios[10]}</option>
-              <option value={ratios[11]}>Hecton - 1:{ratios[11]}</option>
-              <option value={ratios[12]}>Doppelquadrant - 1:{ratios[12]}</option>
+              <option value={ratios[1][0]}>{ratios[1][1]}</option>
+              <option value={ratios[2][0]}>{ratios[2][1]}</option>
+              <option value={ratios[3][0]}>{ratios[3][1]}</option>
+              <option value={ratios[4][0]}>{ratios[4][1]}</option>
+              <option value={ratios[5][0]}>{ratios[5][1]}</option>
+              <option value={ratios[6][0]}>{ratios[6][1]}</option>
+              <option value={ratios[7][0]}>{ratios[7][1]}</option>
+              <option value={ratios[8][0]}>{ratios[8][1]}</option>
+              <option value={ratios[9][0]}>{ratios[9][1]}</option>
+              <option value={ratios[10][0]}>{ratios[10][1]}</option>
+              <option value={ratios[11][0]}>{ratios[11][1]}</option>
+              <option value={ratios[12][0]}>{ratios[12][1]}</option>
               <option disabled>──────────</option>
-              <option value={ratios[13]}>Minor Second - 1:{ratios[13]}</option>
-              <option value={ratios[14]}>Major Second - 1:{ratios[14]}</option>
-              <option value={ratios[15]}>Minor Third - 1:{ratios[15]}</option>
-              <option value={ratios[16]}>Major Third - 1:{ratios[16]}</option>
-              <option value={ratios[17]}>Perfect Fourth - 1:{ratios[17]}</option>
-              <option value={ratios[18]}>Augmented Fourth - 1:{ratios[18]}</option>
-              <option value={ratios[19]}>Perfect Fifth - 1:{ratios[19]}</option>
+              <option value={ratios[13][0]}>{ratios[13][1]}</option>
+              <option value={ratios[14][0]}>{ratios[14][1]}</option>
+              <option value={ratios[15][0]}>{ratios[15][1]}</option>
+              <option value={ratios[16][0]}>{ratios[16][1]}</option>
+              <option value={ratios[17][0]}>{ratios[17][1]}</option>
+              <option value={ratios[18][0]}>{ratios[18][1]}</option>
+              <option value={ratios[19][0]}>{ratios[19][1]}</option>
             </select>
           </div>
           <div className='settings__base'>
@@ -112,7 +112,7 @@ function App() {
                 handleScaleChange(e);
               }}>
               {sizes.map(num => (
-                <option val={num}>{num}</option>
+                <option val={currentScale.length}>{num}</option>
               ))}
             </select>
             {/* <input
