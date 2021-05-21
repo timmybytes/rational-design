08d4cf79d77c3e5a190feffb70f45aa50af9d73c
Timothy Merritt <timmybytes@gmail.com>
2021-05-21 04:25:31 -0700

    (chore): Clean up unneeded placeholders

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Header from './components/Header';
 import SettingsPane from './components/SettingsPane';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
     <>
       <Header />
       <main>
         <SettingsPane />
-
-        {/* TODO: <Examples /> with Context state */}
       </main>
       <Footer />
     </>
