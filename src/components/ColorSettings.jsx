b3456853b684d4ce649a8b2b11e0fef3173db98d
Timothy Merritt <timmybytes@gmail.com>
2021-06-20 07:48:23 -0700

    feat: Add editable color input

--- src/components/ColorSettings.jsx
+++ src/components/ColorSettings.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import { CirclePicker } from 'react-color';
+import { EditableInput } from 'react-color/lib/components/common';
+
+const inputStyles = {
+  input: {
+    border: '2px solid #444444',
+    padding: '5px',
+    borderRadius: '4px',
+    margin: '5px 0',
+  },
+  label: {
+    // fontSize: '1rem',
+    // fontWeight: '800',
+    // width: '100%',
+    display: 'inline-block',
+    color: '#999',
+    padding: '5px',
+  },
+};
 
 const ColorSettings = ({
   colors: { foreground, background },
@@ -7,14 +25,10 @@ const ColorSettings = ({
 }) => {
   const baseColors = [
     '#f44336',
-    '#e91e63',
     '#9c27b0',
-    '#673ab7',
     '#3f51b5',
     '#2196f3',
-    '#03a9f4',
     '#8bbbff',
-    '#009688',
     '#4caf50',
     '#8bc34a',
     '#cddc39',
@@ -23,14 +37,23 @@ const ColorSettings = ({
     '#ff9800',
     '#ff5722',
     '#444444',
-    'fafafa',
+    '#FAFAFA',
   ];
+
   return (
     <div className='settings__colors'>
       <div className='color-container'>
-        <label className='settings__scale-label' htmlFor='bg-color'>
-          BG Color
-        </label>
+        <div class='color-labels'>
+          <label className='settings__scale-label' htmlFor='bg-color'>
+            BG Color
+          </label>
+          <EditableInput
+            style={inputStyles}
+            label='Hex value'
+            value={background}
+            onChange={e => updateColors({ foreground: e.hex, background })}
+          />
+        </div>
         <CirclePicker
           colors={baseColors}
           onChange={e => updateColors({ foreground, background: e.hex })}
@@ -38,9 +61,17 @@ const ColorSettings = ({
         />
       </div>
       <div className='color-container'>
-        <label className='settings__scale-label' htmlFor='text-color'>
-          Text Color
-        </label>
+        <div class='color-labels'>
+          <label className='settings__scale-label' htmlFor='text-color'>
+            Text Color
+          </label>
+          <EditableInput
+            style={inputStyles}
+            label='Hex value'
+            value={foreground}
+            onChange={e => updateColors({ foreground: e.hex, background })}
+          />
+        </div>
         <CirclePicker
           colors={baseColors}
           onChange={e => updateColors({ foreground: e.hex, background })}
